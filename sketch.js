e7925d7006eaa93a90e45452275d7cd6836d9021
SaimaDureja <saimadureja44@gmail.com>
2021-02-09 17:32:31 +0530

    platform

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+var platform;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
@@ -14,7 +14,7 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
+    platform = new Ground(150,300,300,170);
     
     ground = new Ground(600,height,1200,20)
 
@@ -57,6 +57,6 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    platform.display();
     bird.display();
 }
